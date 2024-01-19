109ca05dad93e7a62c23e9130f9de4b83c5f29d8
Akshay Gulhane <argulhane10246@gmail.com>
2024-01-19 23:12:00 +0530

    Commit1901241

--- src/test/java/Day2/DifferentWaysToCreatePostRequestBody.java
+++ src/test/java/Day2/DifferentWaysToCreatePostRequestBody.java
@@ -1,4 +1,51 @@
 package Day2;
+import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
 public class DifferentWaysToCreatePostRequestBody {
+
+    // Post Request body usign HashMap
+    @Test(priority = 1)
+    public void testPostUsingHashMap(){
+        HashMap data = new HashMap();
+        data.put("name","Akshay");
+        data.put("gender","Male");
+        data.put("Physics",34);
+
+        String optSubjects[] = {"C","C++"};
+
+        data.put("optSubjects",optSubjects);
+
+        given()
+                .contentType("application/json")
+                .body(data)
+                .when()
+                .post("http://localhost:8080/")
+                .then()
+                .statusCode(201)
+                .body("name",equalTo("Akshay"))
+                .body("gender",equalTo("male"))
+                .body("optSubjects[0]",equalTo("C"))
+                .body("optSubjects[1]",equalTo("C++"))
+                .header("Content-Type","application/json; charset=utf-8")
+                .log().all()
+        ;
+    }
+    // to Delete the record
+    @Test(priority = 2)
+    void testDelete(){
+        given()
+                .when()
+                .delete("http://localhost:8080/14")
+                .then()
+                .statusCode(200);
+    }
 }
