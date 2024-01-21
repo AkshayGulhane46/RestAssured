6eac3c2c34c07d4e3f9d8ee5ca0dd632f911890e
Akshay Gulhane <argulhane10246@gmail.com>
2024-01-21 23:27:12 +0530

    Commit2201241

--- src/test/java/Day2/DifferentWaysToCreatePostRequestBody.java
+++ src/test/java/Day2/DifferentWaysToCreatePostRequestBody.java
@@ -13,7 +13,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class DifferentWaysToCreatePostRequestBody {
 
     // Post Request body usign HashMap
-    @Test(priority = 1)
+    //@Test(priority = 1)
     public void testPostUsingHashMap(){
         HashMap data = new HashMap();
         data.put("name","Akshay");
@@ -39,6 +39,35 @@ public class DifferentWaysToCreatePostRequestBody {
                 .log().all()
         ;
     }
+
+    //Post a content using json.org
+    @Test(priority = 1)
+    public void testPostUsingJsonOrg(){
+        HashMap data = new HashMap();
+        data.put("name","Akshay");
+        data.put("gender","male");
+        data.put("Physics",34);
+
+        String optSubjects[] = {"C","C++"};
+
+        data.put("optSubjects",optSubjects);
+
+        given()
+                .contentType("application/json")
+                .body(data)
+                .when()
+                .post("http://localhost:8080/students")
+                .then()
+                .statusCode(201)
+                .body("name",equalTo("Akshay"))
+                .body("gender",equalTo("male"))
+                .body("optSubjects[0]",equalTo("C"))
+                .body("optSubjects[1]",equalTo("C++"))
+                .header("Content-Type","application/json")
+                .log().all()
+        ;
+    }
+
     // to Delete the record
     @Test(priority = 2)
     void testDelete(){
